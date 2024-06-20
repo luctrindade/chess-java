e2fc4647ac014634eb1270c52a0d60c366ee2639
Lucas Trindade <lucastrindade1328@gmail.com>
2024-06-20 17:20:28 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,6 @@ public class ChessMatch {
 
     private void initialSetup(){
         board.placePiece(new Rook(board, Color.BLACK), new Position(3,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+        board.placePiece(new King(board, Color.BLACK), new Position(7,4));
     }
 }
