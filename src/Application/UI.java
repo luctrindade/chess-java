cc2df5fb0da8ce707014180150e9857fe5d48172
Lucas Trindade <lucastrindade1328@gmail.com>
2024-06-20 13:19:53 -0300

    Placing pieces on board

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -16,10 +16,10 @@ public class UI {
     }
     private static void printPiece(ChessPiece piece){
         if (piece == null){
-            System.out.print('-');
+            System.out.print("-");
         }
         else{
-            System.out.println(piece);
+            System.out.print(piece);
         }
         System.out.print(" ");
     }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,16 @@
 package chess;
 
 import BoardGame.Board;
+import BoardGame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
@@ -17,5 +21,10 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    private void initialSetup(){
+        board.placePiece(new Rook(board, Color.BLACK), new Position(3,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
     }
 }
