3ef2e7cec0d358f9678d811e88f305393849975a
Lucas Trindade <lucastrindade1328@gmail.com>
2024-06-21 17:09:50 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,8 +23,13 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.BLACK), new Position(3,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,4));
+        placeNewPiece('a', 8, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8,new King(board, Color.BLACK));
+        placeNewPiece('e',1,new King(board, Color.WHITE));
     }
 }
